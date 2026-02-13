7af02c729d1fb2fa15c5aae1ec479ec1c781be66
Viktória Latičová <xlaticova@stuba.sk>
2026-02-13 21:25:48 +0100

    feat: template for filter radio button

--- what-todo/src/components/main/list/TodoFilter.vue
+++ what-todo/src/components/main/list/TodoFilter.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 
+const filter = ref<'all' | 'completed' | 'uncompleted'>('all')
 </script>
 
 <template>
+  <section class="m-4 mt-8 flex justify-start">
 
+    <!-- Radio button for filtering tasks -->
+    <div class="inline-flex gap-4 border border-neutral-200 rounded-lg">
+
+      <!--All tasks -->
+      <label class="flex items-center px-2 py-1 rounded-md"
+             :class="filter === 'all' ? 'text-black bg-neutral-200': 'text-neutral-200 bg-none' "
+      >
+        <input type="radio"
+               name="filter"
+               class="appearance-none"
+               value="all"
+               v-model="filter"
+        />
+        <span>All</span>
+      </label>
+
+      <!-- Completed tasks only -->
+      <label class="flex items-center px-2 py-1 rounded-md"
+             :class="filter === 'completed' ? 'text-black bg-neutral-200': 'text-neutral-200 bg-none' "
+
+      >
+        <input type="radio"
+               name="filter"
+               class="appearance-none"
+               value="completed"
+               v-model="filter"
+        />
+        <span>Completed</span>
+      </label>
+
+      <!-- Uncompleted tasks only -->
+      <label class="flex items-center px-2 py-1 rounded-md"
+             :class="filter === 'uncompleted' ? 'text-black bg-neutral-200': 'text-neutral-200 bg-none' "
+      >
+        <input type="radio"
+               name="filter"
+               class="appearance-none"
+               value="uncompleted"
+               v-model="filter"
+        />
+        <span>Uncompleted</span>
+      </label>
+    </div>
+  </section>
 </template>
 
 <style scoped>
